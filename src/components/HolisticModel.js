04d7f5cb7dd7dec90dd7251019e4be0405100d50
martin mithi <gathitumithi9000@gmail.com>
2023-03-07 01:55:15 +0300

    style: Improve styling

--- src/components/HolisticModel.js
+++ src/components/HolisticModel.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function HolisticModel({index,title, duration, status, text}) {
+function HolisticModel({position,title, duration, status, text}) {
   return (
-    <div className="md:h-52 pb-2 sm:pb-0 h-auto flex flex-col md:flex-row md:items-center bg-[#093331] opacity-80">
+    <div className="md:h-52 pb-2 sm:pb-0 h-auto flex flex-col md:flex-row md:items-center bg-[#093331] opacity-80 mt-4">
       <div className="bg-[#92a788] md:w-[7.5%] md:h-full flex items-center justify-center">
-        {index}
+        {position}
       </div>
       <div className="flex md:w-[25%] md:h-full md:items-center">
         <div className="border-b border-r-0 flex md:flex-col justify-around flex-wrap items-center w-full md:py-8 sm:border-r sm:border-b-0 border-[#c3a955] md:justify-center">
@@ -15,7 +15,7 @@ function HolisticModel({index,title, duration, status, text}) {
           </p>
         </div>
       </div>
-      <p className="md:w-[67.5%] text-sm px-8 pt-2 sm:pt-0">
+      <p className="md:w-[67.5%] text-sm px-4 md:px-8 pt-2 sm:pt-0">
         {text}
       </p>
     </div>
