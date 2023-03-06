5bc82a65460be87d07dcfee91e88e51b1b83fcff
martin mithi <gathitumithi9000@gmail.com>
2023-03-06 17:04:07 +0300

    feat: add Experiences component

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -2,13 +2,15 @@ import React from "react";
 import Hero from "../../components/Hero";
 import LinksList from "../../components/LinksList";
 import EmpowerSection from "./EmpowerSection";
+import Experiences from "./Experiences";
 
 function HomePage() {
   return (
     <div className="">
       <Hero />
       <LinksList />
-      <EmpowerSection/>
+      <EmpowerSection />
+      <Experiences />
     </div>
   );
 }
