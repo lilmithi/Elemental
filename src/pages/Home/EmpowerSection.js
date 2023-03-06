2619ffe53bd1b7308e5ec223adab053a7013543d
martin mithi <gathitumithi9000@gmail.com>
2023-03-06 09:58:20 +0300

    Empower section initial commit

---
+++ src/pages/Home/EmpowerSection.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function EmpowerSection() {
+  return (
+    <section className="h-screen w-full grid md:grid-cols-2">
+      <div>
+        <h1 className="uppercase">Restore. Recharge. Reconnect</h1>
+        <p>
+          It's no secret that we live in unprecedented times. Our world is
+          facing a global mental health epidemic and the challenges thet lie
+          before us demand new approaches to how we lead ourselves and others.
+        </p>
+        <p>
+          Yet, with more to do, less time to do it in, more complexity and
+          moving parts, it's easy to become disconnected and depleted and as a
+          result experience higher levels of stress, anxiety and depression. In
+          fact, according to a recent poll by Forbes, nearly 60% of leaders
+          experience burnout.
+        </p>
+        <p>
+          To help leaders meet these modern-day challenges, we facilitate
+          bespoke, nature-based immersions that offer an inside-out approach to
+          your wellbeing, empowering you to restore and recharge your inner
+          world so you can elevate your impact and influence in the outer world.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default EmpowerSection;
