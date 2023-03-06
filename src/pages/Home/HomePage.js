8a148d4a22506d5bf360b5b2745a16cae7eae2a3
martin mithi <gathitumithi9000@gmail.com>
2023-03-06 08:41:20 +0300

    feat: Add LinksList component to homepage

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Hero from "../../components/Hero";
+import LinksList from "../../components/LinksList";
 
 function HomePage() {
   return (
     <div className="">
       <Hero />
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo voluptas qui autem obcaecati! Fuga iste ipsa minus voluptatibus sed aspernatur.
+      <LinksList />
     </div>
   );
 }
