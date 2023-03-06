56f9c6ec2e5c2003ade5a06c5db6ce16b73f2a62
martin mithi <gathitumithi9000@gmail.com>
2023-03-06 08:39:36 +0300

    feat: Links list initial design

---
+++ src/components/LinksList.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function LinksList() {
+  return (
+    <div className="bg-[#284847]">
+      <ul>
+        <li>Logo</li>
+        <li>Benefits</li>
+        <li>Our Model</li>
+        <li>Retreats</li>
+        <li>Testimonials</li>
+        <li>Team</li>
+        <li>Faq</li>
+        <li>Contact us</li>
+      </ul>
+    </div>
+  );
+}
+
+export default LinksList;
