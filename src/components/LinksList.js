cea803d22673a3ff0b600a0d88acb982457fc897
martin mithi <gathitumithi9000@gmail.com>
2023-03-06 11:14:04 +0300

    style: Change left padding of list

--- src/components/LinksList.js
+++ src/components/LinksList.js
@@ -1,20 +1,41 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
-function LinksList() {
+function NavLinknksNavLinkst() {
   return (
-    <div className="bg-[#284847]">
-      <ul>
-        <li>Logo</li>
-        <li>Benefits</li>
-        <li>Our Model</li>
-        <li>Retreats</li>
-        <li>Testimonials</li>
-        <li>Team</li>
-        <li>Faq</li>
-        <li>Contact us</li>
+    <div className="bg-[#284847] md:px-12">
+      <ul className="flex justify-evenly items-center uppercase text-white py-4 font-RobotoMono font-light text-sm tracking-wide">
+        <NavLink to="/" className="overflow-hidden navLink">
+          <img
+            src="assets/images/mini-logo.png"
+            alt="mini-logo"
+            className="w-10 h-10 brightness-110 object-cover object-center"
+          />
+        </NavLink>
+        <NavLink to="/benefits" className="navLink">
+          Benefits
+        </NavLink>
+        <NavLink to="/model" className="navLink">
+          Our Model
+        </NavLink>
+        <NavLink to="/retreats" className="navLink">
+          Retreats
+        </NavLink>
+        <NavLink to="/testimonials" className="navLink">
+          Testimonials
+        </NavLink>
+        <NavLink to="/team" className="navLink">
+          Team
+        </NavLink>
+        <NavLink to="/faq" className="navLink">
+          Faq
+        </NavLink>
+        <NavLink to="/contacts" className="navLink">
+          Contact us
+        </NavLink>
       </ul>
     </div>
   );
 }
 
-export default LinksList;
+export default NavLinknksNavLinkst;
