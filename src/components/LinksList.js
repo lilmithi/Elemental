4e0f744a8435ee7f4760adabca943bfe1bb3ecdd
martin mithi <gathitumithi9000@gmail.com>
2023-03-06 17:04:59 +0300

    refractor: set navlinks visibility depending on screen width

--- src/components/LinksList.js
+++ src/components/LinksList.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { CiMenuKebab } from "react-icons/ci";
 
 function NavLinknksNavLinkst() {
   return (
     <div className="bg-[#284847] md:px-12">
       <ul className="flex justify-evenly items-center uppercase text-white py-4 font-RobotoMono font-light text-sm tracking-wide">
-        <NavLink to="/" className="overflow-hidden navLink">
+        <NavLink to="/" className="overflow-hidden navLink hidden md:block">
           <img
             src="assets/images/mini-logo.png"
             alt="mini-logo"
@@ -21,18 +22,32 @@ function NavLinknksNavLinkst() {
         <NavLink to="/retreats" className="navLink">
           Retreats
         </NavLink>
-        <NavLink to="/testimonials" className="navLink">
+        <NavLink
+          to="/testimonials"
+          className="navLink hidden md:block optional-navlink"
+        >
           Testimonials
         </NavLink>
-        <NavLink to="/team" className="navLink">
+        <NavLink
+          to="/team"
+          className="navLink hidden md:block optional-navlink"
+        >
           Team
         </NavLink>
-        <NavLink to="/faq" className="navLink">
+        <NavLink to="/faq" className="navLink hidden md:block optional-navlink">
           Faq
         </NavLink>
-        <NavLink to="/contacts" className="navLink">
+        <NavLink
+          to="/contacts"
+          className="navLink hidden md:block optional-navlink"
+        >
           Contact us
         </NavLink>
+        <CiMenuKebab
+          color="rgb(195 169 85)"
+          style={{ border: "solid 1px", fontSize: "1.15rem" }}
+          className="md:hidden"
+        />
       </ul>
     </div>
   );
