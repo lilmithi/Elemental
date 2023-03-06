758a841785c3ba48f13b81e8cca34cbec49b49b7
martin mithi <gathitumithi9000@gmail.com>
2023-03-06 09:58:47 +0300

    Add Empower section component

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../../components/Hero";
 import LinksList from "../../components/LinksList";
+import EmpowerSection from "./EmpowerSection";
 
 function HomePage() {
   return (
     <div className="">
       <Hero />
       <LinksList />
+      <EmpowerSection/>
     </div>
   );
 }
